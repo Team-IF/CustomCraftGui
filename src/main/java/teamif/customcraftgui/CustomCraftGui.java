581cf9fd487287e78e5ba883a2433f844f939567
PatrickKR <mailpatrickkorea@gmail.com>
2020-04-29 15:43:08 +0900

    groovy -> kotlin dsl Added gitignore /build/ Removed unused methods

--- src/main/java/teamif/customcraftgui/CustomCraftGui.java
+++ src/main/java/teamif/customcraftgui/CustomCraftGui.java
@@ -5,16 +5,9 @@ import teamif.customcraftgui.commands.CommandCraftingRoom;
 import teamif.customcraftgui.commands.CommandWorkspace;
 
 public final class CustomCraftGui extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
         this.getCommand("작업실").setExecutor(new CommandCraftingRoom());
         this.getCommand("작업장").setExecutor(new CommandWorkspace());
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
