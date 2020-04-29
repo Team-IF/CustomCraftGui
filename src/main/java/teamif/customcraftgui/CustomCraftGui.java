76e416d47f651a83977ffcb4a55cb66cdf4921a0
Coder-Iro <doohee006@gmail.com>
2020-04-29 12:42:17 +0900

    Create CommandExecutor Class

--- src/main/java/teamif/customcraftgui/CustomCraftGui.java
+++ src/main/java/teamif/customcraftgui/CustomCraftGui.java
@@ -1,13 +1,16 @@
 package teamif.customcraftgui;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import teamif.customcraftgui.commands.CommandCraftingRoom;
+import teamif.customcraftgui.commands.CommandWorkspace;
 
 public final class CustomCraftGui extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("작업실").setExecutor(new CommandCraftingRoom());
+        this.getCommand("작업장").setExecutor(new CommandWorkspace());
     }
 
     @Override
