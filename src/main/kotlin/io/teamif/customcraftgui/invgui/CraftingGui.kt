ece1519d06ec6042fce08c3a12d89613aaf56587
Coder-Iro <doohee006@gmail.com>
2020-05-12 10:02:10 +0900

    Make gui slot Editable

--- src/main/kotlin/io/teamif/customcraftgui/invgui/CraftingGui.kt
+++ src/main/kotlin/io/teamif/customcraftgui/invgui/CraftingGui.kt
@@ -5,6 +5,7 @@ import fr.minuskube.inv.SmartInventory
 import fr.minuskube.inv.SmartInventory.builder
 import fr.minuskube.inv.content.InventoryContents
 import fr.minuskube.inv.content.InventoryProvider
+import fr.minuskube.inv.content.SlotPos
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -20,7 +21,22 @@ class CraftingGui : InventoryProvider {
     }
 
     override fun init(player: Player, contents: InventoryContents) {
+        val slots = arrayOf(
+                SlotPos(2, 2),
+                SlotPos(2, 3),
+                SlotPos(2, 4),
+                SlotPos(3, 2),
+                SlotPos(3, 3),
+                SlotPos(3, 4),
+                SlotPos(4, 2),
+                SlotPos(4, 3),
+                SlotPos(4, 4),
+                SlotPos(3, 8)
+        )
         contents.fillBorders(ClickableItem.empty(ItemStack(Material.STAINED_GLASS_PANE)))
+        for (slot in slots) {
+            contents.setEditable(slot, true)
+        }
     }
 
     override fun update(player: Player, contents: InventoryContents) {}
